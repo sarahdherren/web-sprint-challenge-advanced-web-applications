ba81bd6c7e26ab044d6d31bac2b5d498f9406295
Sarah D Herren <herren.sarah@icloud.com>
2021-08-14 12:33:01 -0500

    logout functionality built

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import BubblePage from "./components/BubblePage";
+import { axiosWithAuth } from "./helpers/axiosWithAuth";
 import "./styles.scss";
 
 function App() {
+
+  const handleSubmit = (e) => {
+    axiosWithAuth()
+      .post("/logout")
+      .then(res => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      })
+      .catch(err => {
+        console.log("user must be logged-in to logout:", err);
+      })
+
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a data-testid="logoutButton" onClick={handleSubmit} href="#">logout</a>
         </header>
       <Switch>
         <PrivateRoute exact path="/protected" component={BubblePage} />
